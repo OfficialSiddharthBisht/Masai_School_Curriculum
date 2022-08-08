0e9358f0638692d7dcf62a5cec1548dec7be383c
Akash Kumawat <akashkmt963@gmail.com>
2022-08-09 00:56:52 +0530

    all features done

--- JS Prep/city-country/src/Components/Home/Home.jsx
+++ JS Prep/city-country/src/Components/Home/Home.jsx
@@ -1,48 +1,71 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { filterByCountry, getDataCities, sortByPopulation } from '../../Redux/Cities/action';
-import { getDataCountries } from '../../Redux/Countries/action';
+import { Button } from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  filterByCountry,
+  getDataCities,
+  sortByPopulation,
+} from "../../Redux/Cities/action";
+import { getDataCountries } from "../../Redux/Countries/action";
+import TransitionsModal from "../EditModal/EditModal";
 
 function Home() {
   const dispatch = useDispatch();
-  const {cities} = useSelector(store => store.cities);
-  const {countries} = useSelector(store => store.countries);
-  const [enteredCountry, setEnteredCountry] = React.useState('');
+  const { cities } = useSelector((store) => store.cities);
+  const { countries } = useSelector((store) => store.countries);
+  const [enteredCountry, setEnteredCountry] = React.useState("");
   React.useEffect(() => {
     dispatch(getDataCities());
     dispatch(getDataCountries());
-  },[dispatch]);
+  }, [dispatch]);
 
-  const handleDelete = async(id) => {
+  const handleDelete = async (id) => {
     try {
       await fetch(`http://localhost:8080/cities/${id}`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       });
       dispatch(getDataCities());
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
 
   return (
     <div>
-      <select name="sort" id="sort" onChange={(e)=>{dispatch(sortByPopulation(e.target.value, enteredCountry))}}>
-        <option value="">Sort By Population</option>
-        <option value="asc">Ascending</option>
-        <option value="desc">Descending</option>
-      </select>
-      <select name="" id="country" value={enteredCountry} onChange={(e)=>{setEnteredCountry(e.target.value); dispatch(filterByCountry(e.target.value))}}>
+      <div style={{margin:"auto", display:'flex', justifyContent:"space-evenly", marginTop:'20px'}}>
+        <select style={{padding:"5px"}}
+          name="sort"
+          id="sort"
+          onChange={(e) => {
+            dispatch(sortByPopulation(e.target.value, enteredCountry));
+          }}
+        >
+          <option value="">Sort By Population</option>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+        <select
+          name=""
+          id="country"
+          value={enteredCountry}
+          onChange={(e) => {
+            setEnteredCountry(e.target.value);
+            dispatch(filterByCountry(e.target.value));
+          }}
+        >
           <option value="">Select Country</option>
-          {countries?.map(country => (
-            <option key={country.id} value={country.country}>{country.country}</option>
+          {countries?.map((country) => (
+            <option key={country.id} value={country.country}>
+              {country.country}
+            </option>
           ))}
         </select>
-      <table className='table'>
-        <thead >
+      </div>
+      <table className="table">
+        <thead>
           <tr>
             <th>Id</th>
             <th>Country</th>
@@ -53,20 +76,30 @@ function Home() {
           </tr>
         </thead>
         <tbody>
-          {cities.map((city,i) => (
+          {cities.map((city, i) => (
             <tr key={city.id}>
-              <td>{i+1}</td>
+              <td>{i + 1}</td>
               <td>{city.country}</td>
               <td>{city.city}</td>
               <td>{city.population}</td>
-              <td><button>Edit</button></td>
-              <td><button onClick={()=>{handleDelete(city.id)}}>Delete</button></td>
+              <td>
+                <TransitionsModal item={city}/>
+              </td>
+              <td>
+                <Button
+                  onClick={() => {
+                    handleDelete(city.id);
+                  }}
+                >
+                  Delete
+                </Button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
